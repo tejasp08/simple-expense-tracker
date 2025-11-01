00ec1a2fd12de4bd28d7c7266b4f470be8417236
tejasp08 <tejasp482003@gmail.com>
2025-11-01 20:20:32 +0530

    Add File

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,3 +1,40 @@
 public class Transaction {
+    //Fields
+    String date;
+    String type;
+    int amount;
+    String description;
+
+    //Constructor
+    public Transaction(String date, String type, int amount, String description) {
+        this.date = date;
+        this.type = type;
+        this.amount = amount;
+        this.description = description;
+    }
     
+    //getters
+    public String getDate() {
+        return date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    //toString method
+    @Override
+    public String toString() {
+        return "Transaction [date=" + date + ", type=" + type + ", amount=" + amount + ", description=" + description
+                + "]";
+    }
+
 }
