35274395351fa237a25815f250ed2f8ce78e85ad
tejasp08 <tejasp482003@gmail.com>
2025-11-01 21:55:18 +0530

    Update File

--- src/ExpenseTracker.java
+++ src/ExpenseTracker.java
@@ -5,6 +5,38 @@ public class ExpenseTracker {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String terminateChoice;
+
+        System.out.println("Personal Expense Tracker:)");
+
+        do {
+            System.out.println("What do you want to do ?");
+            System.out.println("1. Add Transaction");
+            System.out.println("2. View All Transaction");
+            System.out.println("3. View Summary");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("add");
+                    break;
+                case 2:
+                    System.out.println("view");
+                    break;
+                case 3:
+                    System.out.println("summary");
+                    break;
+                default:
+                    System.out.println("Enter a valid choice!");
+                    break;
+            }
+
+            System.out.println("Do you want to continue ? (Yes/No)");
+            terminateChoice = sc.next().toLowerCase();
+
+        } while (!terminateChoice.equals("no"));
         
+        System.out.println("Thanks:)");
+
     }
 }
