47f708beab71ef24b2f47b15be8d543eb78868b6
tejasp08 <tejasp482003@gmail.com>
2025-11-02 16:46:55 +0530

    Updated file

--- src/ExpenseTracker.java
+++ src/ExpenseTracker.java
@@ -1,13 +1,22 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ExpenseTracker {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // String fileName = "expenses.txt";
 
         String terminateChoice;
+        String date;
+        String type;
+        int amount;
+        String description;
 
-        System.out.println("Personal Expense Tracker:)");
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        
+
+        System.out.println("---Personal Expense Tracker:)---");
 
         do {
             System.out.println("What do you want to do ?");
@@ -18,10 +27,33 @@ public class ExpenseTracker {
 
             switch (choice) {
                 case 1:
-                    System.out.println("add");
-                    break;
+                while (true) {
+                    System.out.println("Adding Transaction");
+                    System.out.println("Format: DD-MM-YYYY, Type, Amount, Description");
+                    System.out.print("Enter Date: ");
+                    date = sc.next();
+                    System.out.print("Enter Type: ");
+                    type = sc.next();
+                    System.out.print("Enter Amount: ");
+                    amount = sc.nextInt();
+                    System.out.print("Enter Description: ");
+                    description = sc.next();
+                    
+                    Transaction t = new Transaction(date, type, amount, description);
+                    System.out.println("Transaction added: " + t);
+                    //addTransaction(date, type, amount, description);
+                    transactions.add(t);
+                    System.out.println();
+                    System.out.println("Do you want to add another transaction ? (yes/no)");
+                    String choice2 = sc.next().toLowerCase();
+
+                    if (choice2.equals("no")) {
+                        break;
+                    }
+                }
+                break;
                 case 2:
-                    System.out.println("view");
+                    System.out.println(transactions);
                     break;
                 case 3:
                     System.out.println("summary");
@@ -36,7 +68,16 @@ public class ExpenseTracker {
 
         } while (!terminateChoice.equals("no"));
         
-        System.out.println("Thanks:)");
+        System.out.println("Exiting program. Goodbye!");
 
+    }
+
+    //Methods
+    private static void addTransaction() {
+
+    }
+
+    private static void viewTransaction() {
+        
     }
 }
